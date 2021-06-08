803eda79a429780d668f687bd8c14e307973cc4e
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-08 10:28:51 -0700

    capitalizeFilter

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+/* eslint-disable keyword-spacing */
 export default ({ name }) => {
   return name;
 };
@@ -8,3 +9,14 @@ export function copyAndPush(arr, x) {
   return [arr, copyArr];
 }
 
+export function capitalizeAndFilter(arr) {
+  const fixedArr = [];
+  for (const index in arr) {
+    const capitalizedString = arr[index].toUpperCase();
+    if (capitalizedString[0] !== 'F') {
+      fixedArr[fixedArr.length] = capitalizedString;
+    }
+  }
+
+  return fixedArr;
+}
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,5 @@
 import getName from './functions';
-import { copyAndPush } from './functions';
+import { copyAndPush, capitalizeAndFilter } from './functions';
 
 describe('getName', () => {
   it('returns name property of an object', () => {
@@ -14,5 +14,13 @@ describe('copyAndPush', () => {
     const numbers = [1, 2, 3];
     const newArray = copyAndPush(numbers, 4);
     expect(newArray).toEqual([numbers, [1, 2, 3, 4]]);
+  });
+});
+
+describe('capatalizeFilter', () => {
+  it('takes an array of strings, capatalizes all strings, and filters out strings that start w/ F/f', () => {
+    const sampleString = ['Frankie says relax', 'xanadou', 'forget about it'];
+    const noFStrings = capitalizeAndFilter(sampleString);
+    expect(noFStrings).toEqual(['XANADOU']);
   });
 });
