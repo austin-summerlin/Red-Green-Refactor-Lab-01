4d70f2574be5268566b207a5b48ed8883ba27a9d
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-07 17:18:43 -0700

    first getName test

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,9 @@
-import getName from './redGreenRefactor.js';
+import getName from './functions';
 
 describe('getName', () => {
   it('returns name property of an object', () => {
     const spot = { name: 'spot', age: 5, weight: '20 lbs' };
     const name = getName(spot);
-    expect(name).toEqual('Spot');
+    expect(name).toEqual('spot');
   });
 });
